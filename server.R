05e80ca71e8ad0784a3ba2b80d9b23a7f7cd6c20
Liam Sexton <liams4@uw.edu>
2019-06-01 15:19:11 -0700

    Filtered data

--- server.R
+++ server.R
@@ -1,7 +1,26 @@
 library(shiny)
+library(dplyr)
 
 data <- read.csv("data/states_all_extended.csv")
 
+spending_data <- data %>% select(STATE, YEAR, TOTAL_REVENUE, FEDERAL_REVENUE, 
+                                 LOCAL_REVENUE, TOTAL_EXPENDITURE, 
+                                 AVG_MATH_4_SCORE, AVG_MATH_8_SCORE, 
+                                 AVG_READING_4_SCORE, AVG_READING_8_SCORE) %>% 
+                          filter(STATE != "AMERICAN_SAMOA", STATE != "BIE", 
+                                 STATE != "DOD_DOMESTIC", STATE != "DOD_OVERSEAS",
+                                 STATE != "GUAM", STATE != "NORTHERN_MARIANAS",
+                                 STATE != "PUERTO_RICO", STATE != "VIRGIN_ISLANDS",
+                                 STATE != "BUREAU_OF_INDIAN_EDUCATION", 
+                                 STATE != "DEPARTMENT_OF_DEFENSE_EDUCATION_ACTIVITY",
+                                 STATE != "U.S._VIRGIN_ISLANDS", 
+                                 STATE != "DD", STATE != "BI", 
+                                 STATE != "NORTHERN_MARIANA_ISLANDS")
+
+spending_data_2005 <- spending_data %>% filter(YEAR == 2005)
+spending_data_2011 <- spending_data %>% filter(YEAR == 2011)
+spending_data_2015 <- spending_data %>% filter(YEAR == 2015)
+
 shinyServer(function(input, output) {
-  
+ 
 })
