9f21b300f713c1dc22d0b8a526a39341142e425b
Liam Sexton <liams4@uw.edu>
2019-06-04 15:54:10 -0700

    Readded old changes resolves #2

--- server.R
+++ server.R
@@ -27,6 +27,25 @@ spending_data_2005 <- spending_data %>% filter(YEAR == 2005)
 spending_data_2011 <- spending_data %>% filter(YEAR == 2011)
 spending_data_2015 <- spending_data %>% filter(YEAR == 2015)
 
+race_data <- data %>% 
+  select(STATE, YEAR, ENROLL, GRADES_ALL_AS, GRADES_ALL_AM, 
+         GRADES_ALL_HI, GRADES_ALL_BL, GRADES_ALL_WH, GRADES_ALL_HP, GRADES_ALL_TR)  %>% 
+  filter(YEAR == "2015") %>% 
+  filter(STATE != "AMERICAN_SAMOA", STATE != "BUREAU_OF_INDIAN_EDUCATION", STATE != "DEPARTMENT_OF_DEFENSE_EDUCATION_ACTIVITY",
+         STATE != "GUAM", STATE != "PUERTO_RICO", STATE != "U.S._VIRGIN_ISLANDS") %>% 
+  mutate(GRADES_ALL_AS = ifelse(!is.na(GRADES_ALL_AS), GRADES_ALL_AS, "-")) %>% 
+  mutate(GRADES_ALL_AM = ifelse(!is.na(GRADES_ALL_AM), GRADES_ALL_AM, "-")) %>% 
+  mutate(GRADES_ALL_HI = ifelse(!is.na(GRADES_ALL_HI), GRADES_ALL_HI, "-")) %>% 
+  mutate(GRADES_ALL_BL = ifelse(!is.na(GRADES_ALL_BL), GRADES_ALL_BL, "-")) %>% 
+  mutate(GRADES_ALL_WH = ifelse(!is.na(GRADES_ALL_WH), GRADES_ALL_WH, "-")) %>% 
+  mutate(GRADES_ALL_HP = ifelse(!is.na(GRADES_ALL_HP), GRADES_ALL_HP, "-")) %>% 
+  mutate(GRADES_ALL_TR = ifelse(!is.na(GRADES_ALL_TR), GRADES_ALL_TR, "-")) %>% 
+  rename(Asian = "GRADES_ALL_AS", American_Indian_or_Alaska_Native = "GRADES_ALL_AM", Hispanic_or_Latino = "GRADES_ALL_HI", 
+         Black = "GRADES_ALL_BL", White = "GRADES_ALL_WH", Hawaiian_Native_or_Pacific_Islander = "GRADES_ALL_HP",
+         Two_or_More_Races = "GRADES_ALL_TR", State = "STATE", Total_Enrollment = "ENROLL") %>% 
+  select(-YEAR)
+
+
 
 shinyServer(function(input, output) {
   
@@ -46,27 +65,33 @@ shinyServer(function(input, output) {
       coord_fixed(1.3)
   })
   
+  # Gets all of education spending the data specific to the year passed as a 
+  # parameter
+  get_year_spending_data <- function(year) {
+    spending_data <- spending_data_2005
+    if (year == 2011) {
+      spending_data <- spending_data_2011
+    } else if (year == 2015) {
+      spending_data <- spending_data_2015
+    } 
+    spending_data
+  }
+  
+  # Gets all of the revenue data for the specific year passed as a parameter
+  get_revenue_data <- function(year) {
+    revenue_data <- total_revenue_by_state$REVENUE_2005
+    if (year == 2011) {
+      revenue_data <- total_revenue_by_state$REVENUE_2011
+    } else if (year == 2015) {
+      revenue_data <- total_revenue_by_state$REVENUE_2015
+    } 
+    revenue_data
+  }
+  
   #Server stuff for race data
   state <- read.csv("data/states.csv", sep=",") %>% 
     mutate(State = tolower(State)) %>% 
     mutate(State = str_replace_all(State, " ", "_"))
-  race_data <- data %>% 
-    select(STATE, YEAR, ENROLL, GRADES_ALL_AS, GRADES_ALL_AM, 
-           GRADES_ALL_HI, GRADES_ALL_BL, GRADES_ALL_WH, GRADES_ALL_HP, GRADES_ALL_TR)  %>% 
-    filter(YEAR == "2015") %>% 
-    filter(STATE != "AMERICAN_SAMOA", STATE != "BUREAU_OF_INDIAN_EDUCATION", STATE != "DEPARTMENT_OF_DEFENSE_EDUCATION_ACTIVITY",
-           STATE != "GUAM", STATE != "PUERTO_RICO", STATE != "U.S._VIRGIN_ISLANDS") %>% 
-    mutate(GRADES_ALL_AS = ifelse(!is.na(GRADES_ALL_AS), GRADES_ALL_AS, "-")) %>% 
-    mutate(GRADES_ALL_AM = ifelse(!is.na(GRADES_ALL_AM), GRADES_ALL_AM, "-")) %>% 
-    mutate(GRADES_ALL_HI = ifelse(!is.na(GRADES_ALL_HI), GRADES_ALL_HI, "-")) %>% 
-    mutate(GRADES_ALL_BL = ifelse(!is.na(GRADES_ALL_BL), GRADES_ALL_BL, "-")) %>% 
-    mutate(GRADES_ALL_WH = ifelse(!is.na(GRADES_ALL_WH), GRADES_ALL_WH, "-")) %>% 
-    mutate(GRADES_ALL_HP = ifelse(!is.na(GRADES_ALL_HP), GRADES_ALL_HP, "-")) %>% 
-    mutate(GRADES_ALL_TR = ifelse(!is.na(GRADES_ALL_TR), GRADES_ALL_TR, "-")) %>% 
-    rename(Asian = "GRADES_ALL_AS", American_Indian_or_Alaska_Native = "GRADES_ALL_AM", Hispanic_or_Latino = "GRADES_ALL_HI", 
-           Black = "GRADES_ALL_BL", White = "GRADES_ALL_WH", Hawaiian_Native_or_Pacific_Islander = "GRADES_ALL_HP",
-           Two_or_More_Races = "GRADES_ALL_TR", State = "STATE", Total_Enrollment = "ENROLL") %>% 
-    select(-YEAR)
   
   race_data_tidied <- race_data %>% 
     gather(key = state,
